57770309a086d2e76a1c3b1ed2bc017e008472ac
Eurus001 <86990592+Eurus001@users.noreply.github.com>
2023-07-09 23:52:10 -0500

    Funciones de lectura
    Añadidas funciones de lectura al programa

--- SistemaCalificaciones.c
+++ SistemaCalificaciones.c
@@ -84,3 +84,125 @@ int main()
     free(alumnos);
     return 0;
 }
+
+void MenuInicial() {
+    printf("Ingrese el numero de materias que hay: ");
+    scanf_s("%d", &n_materiasTotal);
+    printf("%d\n",n_materiasTotal);
+    printf("Ingrese el numero de progresos que hay: ");
+    scanf_s("%d", &n_progresosTotal);
+    printf("%d\n",n_progresosTotal);
+    ModificarMetadatos();
+    fprintf_s(archivo, "\n");
+}
+
+//Modifica los metadatos de la base de datos (primera linea)
+// 0 es materias, 1 es progresos y 2 es numero de alumnos
+void ModificarMetadatos()
+{
+    fseek(archivo, 0, SEEK_SET);
+    fprintf_s(archivo, "%d;%d;%d\n", n_materiasTotal, n_progresosTotal, n_alumnosTotal);
+}
+
+void CargaDeDatos() {
+    char linea[200];
+
+    if (!archivo) {
+        printf("Error al abrir el archivo.\n");
+        return;
+    }
+
+    if (fgets(linea, sizeof(linea), archivo) == NULL) {
+        printf("Error al leer la línea de datos iniciales.\n");
+        return;
+    }
+
+    if (sscanf_s(linea, "%d;%d;%d", &n_materiasTotal, &n_progresosTotal, &n_alumnosTotal) != 3) {
+        printf("Error al leer los valores iniciales.\n");
+        return;
+    }
+
+    alumnos = (struct Alumno*)malloc(n_alumnosTotal * sizeof(struct Alumno));
+    if (alumnos == NULL) {
+        printf("Error al asignar memoria.\n");
+        return;
+    }
+
+    for (int i = 0; i < n_alumnosTotal; i++) {
+        if (fgets(linea, sizeof(linea), archivo) != NULL) {
+            // Eliminar el carácter de nueva línea al final de la línea leída
+            linea[strcspn(linea, "\n")] = '\0';
+            ProcesarEstudiante(linea, i);
+        }
+        else {
+            printf("Error al leer la línea del estudiante %d.\n", i + 1);
+        }
+    }
+}
+
+void ProcesarEstudiante(const char* linea, const int i) {
+    char nombre[50], hora[20]; // Búfer para almacenar el nombre
+
+    // Leer los datos del estudiante de la línea
+    if (sscanf_s(linea, "%19[^;];%49[^;];%d", hora, sizeof(hora), nombre, sizeof(nombre), &alumnos[i].edad) != 3) {
+        printf("Error al leer los valores iniciales del estudiante.\n");
+        return;
+    }
+    strcpy_s(alumnos[i].horaMod, sizeof(alumnos[i].horaMod), hora);
+    alumnos[i].nombre = _strdup(nombre);
+
+    // Inicializar el array de promedios
+    alumnos[i].promedios = (float**)malloc(n_materiasTotal * sizeof(float*));
+    if (alumnos[i].promedios == NULL) {
+        printf("Error al asignar memoria para los promedios del estudiante.\n");
+        return;
+    }
+
+    for (int j = 0; j < n_materiasTotal; j++) {
+        alumnos[i].promedios[j] = (float*)malloc(n_progresosTotal * sizeof(float));
+        if (alumnos[i].promedios[j] == NULL) {
+            printf("Error al asignar memoria para los promedios de la materia %d del estudiante.\n", j + 1);
+
+            // Liberar memoria asignada previamente
+            for (int k = 0; k < j; k++) {
+                free(alumnos[i].promedios[k]);
+            }
+            free(alumnos[i].promedios);
+
+            return;
+        }
+    }
+    // Ignorar los primeros tres tokens
+    int tokensIgnorados = 0;
+    const char* token = strchr(linea, ';'); // Buscar el primer punto y coma
+    while (tokensIgnorados < 2 && token != NULL) {
+        token++;
+        token = strchr(token, ';');
+        tokensIgnorados++;
+    }
+
+    // Leer los promedios de la línea
+    for (int j = 0; j < n_materiasTotal; j++) {
+        for (int k = 0; k < n_progresosTotal; k++) {
+            if (token != NULL) {
+                token++; // Avanzar al siguiente carácter después del punto y coma
+
+                // Convertir directamente el token a float
+                float promedio;
+                if (sscanf_s(token, "%f", &promedio) != 1) {
+                    printf("Error al leer los promedios de la materia %d del estudiante.\n", j + 1);
+                    return;
+                }
+                alumnos[i].promedios[j][k] = promedio;
+            }
+            else {
+                printf("Error al leer los promedios de la materia %d del estudiante.\n", j + 1);
+                return;
+            }
+
+            token = strchr(token, ';'); // Buscar el siguiente punto y coma
+        }
+    }
+
+}
+
